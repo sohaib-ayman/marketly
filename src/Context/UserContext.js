e8369bfd20ef92d977204c2cc432cf96d712d04b
Rawan-Hamdi <rhamdi496@gmail.com>
2026-04-25 23:34:01 +0300

    fix: cart improvements

--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,24 +1,26 @@
 import { onAuthStateChanged, signInAnonymously } from "firebase/auth";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { auth } from "../firebase";
 import { store } from "../Store/store";
-import { mergeCart, clearCart } from "../Store/cartSlice";
+import { mergeCart } from "../Store/cartSlice";
 
 export let UserContext = createContext();
 
 export default function UserContextProvider(props) {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+    const previousUser = useRef(null);
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
             if (currentUser) {
-                if (!currentUser.isAnonymous) {
+                if (!currentUser.isAnonymous && previousUser.current?.isAnonymous) {
                     let guestCart = store.getState().cart.items;
                     if (guestCart.length > 0) {
                         store.dispatch(mergeCart(guestCart));
                     }
                 }
+                previousUser.current = currentUser;
                 setUser(currentUser);
             } else {
                 const result = await signInAnonymously(auth);
